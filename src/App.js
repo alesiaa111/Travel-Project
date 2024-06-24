39c4bca4193311bf841da0f1f31f378f13a110c0
Alesia <davyd4vaalehar@yandex.ru>
2024-06-24 18:13:56 +0300

    my master

--- src/App.js
+++ src/App.js
@@ -2,13 +2,29 @@ import React from "react";
 import { Header } from "./views/header/Header.js";
 import { InfoContent } from "./views/info-content/InfoContent.js";
 import { Footer } from "./views/Footer.js";
+// import { Button } from '.components/button/Buton.js'
 
 export const App = () => {
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Rivers/>,
+    },
+    {
+      path: "product/:productId",
+      element: <Rent/>,
+  },
+  ]);
+
+
+
   return (
-    <div>
+    <div className={appContainer}>
       <Header />
       <InfoContent/>
       <Footer/>
+      <RouterProvider router={router} />
     </div>
   );
 };
--- src/views/header/Header.js
+++ src/views/header/Header.js
@@ -1,11 +1,12 @@
 import React from "react";
 import styles from "./heder.module.css";
+import { Logo } from "./assets/Logo.png";
 
 
 export const Header = () => {
   return (
     <div className={styles.firstScreen}>
-      <img src="/assets/Logo.png" alt="Logo" />
+      <img src={Logo} alt="Logo" />
       <div className={styles.firstScreen_title}>
         <h1>Организация сплавов</h1>
         <h2 className={styles.phone_title}>
