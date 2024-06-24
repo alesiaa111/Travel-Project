c633976945c1d54cbff4fbb9c954b575b444985f
Alesia <davyd4vaalehar@yandex.ru>
2024-06-24 20:43:57 +0300

    my feature

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
 import React from "react";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import { Header } from "./Header.js";
 import { Content } from "./Content.js";
-// import { Footer } from "./views/Footer.js";
+import { Footer } from "./Footer.js";
+import { Rivers } from './Rivers.js';
+import { Rent } from './Rent.js';
 
 export const App = () => {
-  return (
-    <div>
-      <Header />
-      <Content/>
-      {/* <Footer/> */}
-    </div>
-  );
-};
+
+
+    const router = createBrowserRouter([
+      {
+        path: "rivers/",
+        element: <Rivers />,
+      },
+      {
+        path: "rent/",
+        element: <Rent />,
+    },
+    ]);
+  
+  
+    return (
+      <div>
+        <Header />
+        <Content />
+        <Footer />
+        <RouterProvider router={router} />
+      </div>
+    );
+  };
+ 
--- src/Header.js
+++ src/Header.js
@@ -1,14 +1,18 @@
 import React from "react";
 import styles from "./index.css";
+import Logo from './assets/Logo.png'
 
 
 export const Header = () => {
+
+
+
   return (
-    <div className={styles.firstScreen}>
-      <img src="/assets/Logo.png" alt="Logo" />
-      <div className={styles.firstScreen_title}>
+    <div>
+      <img src={Logo} alt="Logo" />
+      <div>
         <h1>Организация сплавов</h1>
-        <h2 className={styles.phone_title}>
+        <h2>
           +375(25) 655-54-05 <br />
           +375(29) 114-52-20
         </h2>
