712534519b1885abc71a6ddf7d90b7b92c71b820
Alesia <davyd4vaalehar@yandex.ru>
2024-06-24 16:09:38 +0300

    my master

--- src/views/info-content/InfoContent.js
+++ src/views/info-content/InfoContent.js
@@ -1,10 +1,12 @@
 import React from "react";
 import styles from "./info-content.module.css";
+import { Button } from "./components/button";
 
 export const InfoContent = () => {
     return (
-        <div>
+        <div className={styles.wraper}>
             <h3> Сборные сплавы </h3>
+            <Button />
         </div>
       );
 };
