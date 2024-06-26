ea7e4c1c3dab08d91a2aa5e1fb310adc0809deab
Alesia <davyd4vaalehar@yandex.ru>
2024-06-26 16:27:55 +0300

    my master

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,7 @@ import styles from "./header.module.css"
 
 export const Header = () => {
   return (
-    <div className={styles.firstTitle}>
+    <div className={styles.firstScreen}>
       <img src={require("./images/Logo.png")} alt="Logo" />
       <h1> Организация сплавов в Гомеле</h1>
     </div>
