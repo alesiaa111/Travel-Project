b3f6147292f31a896dfd3688cd37a56952f488bd
Alesia <davyd4vaalehar@yandex.ru>
2024-07-31 14:25:36 +0300

    main

--- src/views/footer/index.js
+++ src/views/footer/index.js
@@ -1,38 +1,49 @@
 import React from "react";
-import logoImg from '../../images/Logo.png';
+import logoImg from "../../images/Logo.png";
 import styles from "./index.module.css";
-import { Instagram, YouTube } from "@mui/icons-material";
+import { Instagram, YouTube, Phone, Email } from "@mui/icons-material";
 
 // TODO: все require поменять на import
 
-
 export const Footer = () => {
-    return (
-      <footer>
-        <div className={styles.footer_wrapper}>
-          <div className={styles.logo_end}>
-            <img src={logoImg} alt="Сплавы в Гомеле" />
-          </div>
-          <div className={styles.contact}>
-            <p>ИП Давыдов А.В.</p>
-            <p>УНП 491683235</p>
-            <p>Life +375(25) 655-54-05</p>
-            <p> A1 +375(29) 114-52-20</p>
-          </div>
-          <div className={styles.footer_favicon}>
-            <div className={styles.favicon}> 
-              <a href="https://instagram.com/avantyuristy.by?utm_source=qr&igshid=MzNlNGNkZWQ4Mg==">
-                <Instagram  htmlColor="#000000"/>
-              </a>
-            </div>
-            <div className={styles.favicon}>
-              <a href="https://www.youtube.com/@avantyuristy.by1234">
-                <YouTube  htmlColor="#000000"/>
-              </a>
-            </div>
-          </div>
-          <div className={styles.footer_bottom}>Гомель © 2024</div>
+  return (
+    <footer>
+      <div className={styles.footer_wrapper}>
+        <div className={styles.logo_end}>
+          <img src={logoImg} alt="Сплавы в Гомеле" />
         </div>
-      </footer>
-    );
+        <div className={styles.contact}>
+          <p>ИП Давыдов А.В.</p>
+          <p>УНП 491683235</p>
+          <div>
+            <div className={styles.phone}>
+              <Phone />
+              <a href="tel:+375256555405">  Life +375 (25) 655-54-05</a>
+            </div>
+            <div className={styles.phone}>
+              <Phone />
+              <a href="tel:+375291145220">  A1 +375(29) 114-52-20</a>
+            </div>
+            <div className={styles.email}>
+              <Email />
+              <a href="mailto:avantyuristyTravel@yandex.ru"> avantyuristyTravel@yandex.ru</a>
+            </div>
+          </div>
+        </div>
+        <div className={styles.footer_favicon}>
+          <div className={styles.favicon}>
+            <a href="https://instagram.com/avantyuristy.by?utm_source=qr&igshid=MzNlNGNkZWQ4Mg==">
+              <Instagram htmlColor="#000000" />
+            </a>
+          </div>
+          <div className={styles.favicon}>
+            <a href="https://www.youtube.com/@avantyuristy.by1234">
+              <YouTube htmlColor="#000000" />
+            </a>
+          </div>
+        </div>
+        <div className={styles.footer_bottom}>Гомель © 2024</div>
+      </div>
+    </footer>
+  );
 };
--- src/views/rent/index.js
+++ src/views/rent/index.js
@@ -1,4 +1,3 @@
-import Resct from "react";
 import styles from "./index.module.css";
 
 export const Rent = () => {
@@ -7,13 +6,27 @@ export const Rent = () => {
       <div className={styles.rent}>
         <h3 className={styles.rent_title}>Аренда байдарок и снаряжения</h3>
         <div className={styles.rent_text}>
-          Аренда байдарок и туристического снаряжения (спасательные жилеты,
-          палатки, туристические коврики, спальные мешки).
-          <p>
-            Стоимость аренды байдарки + спасательный жилет + весла -
-            <span className={styles.price}>50 руб.</span>за сутки. Возможна
-            доставка или самовывоз.
-          </p>
+          Аренда байдарок и туристического снаряжения:
+          <ul className={styles.li}>
+            <li>
+              ✔ Байдарка + спасательный жилет + весла - 
+              <span className={styles.price}>50 руб.</span>за сутки.
+            </li>
+            <li>
+              ✔ Сапборд + спасательный жилет + весла - 
+              <span className={styles.price}>40 руб.</span>за сутки.
+            </li>
+            <li>
+              ✔ Катамаран (сборка катамарана включена в стоимость) + спасательный
+              жилет + весла - <span className={styles.price}>100 руб.</span>за
+              сутки.
+            </li>
+            <li className={styles.li_big}>
+              Каждые последующие сутки аренды скидка -
+              <span className={styles.price}>10 руб.</span>за сутки.
+            </li>
+          </ul>
+          <p>Возможна доставка или самовывоз.</p>
           <p>
             Стоимость доставки расчитывается.
             <span className={styles.price}>1 км - 1 руб.</span>
