6f9f9141c20d12565af939f3238cce1760212ec8
Alesia <davyd4vaalehar@yandex.ru>
2024-08-10 23:20:17 +0300

    auth

---
+++ src/views/admin/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Title } from "../../components/title";
+import { Center } from "../../components/center";
+import styles from "./index.module.css";
+
+export const Admin = () => {
+  return (
+    <>
+      <Center>
+        <Title size="xl" title="Hello word" />
+       
+      </Center>
+    </>
+  );
+};
---
+++ src/views/firebase-auth/index.js
@@ -0,0 +1,32 @@
+import React, { useEffect } from 'react';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import * as firebaseui from 'firebaseui';
+import { auth } from '../firebase';
+
+export const FirebaseAuth = () => {
+  useEffect(() => {
+    const ui = new firebaseui.auth.AuthUI(auth);
+
+    const uiConfig = {
+      signInSuccessUrl: '/admin',
+      signInOptions: [
+        {
+          provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+          requireDisplayName: false,
+        },
+       
+      ],
+    };
+
+    ui.start('#firebaseui-auth-container', uiConfig);
+
+    return () => {
+      ui.delete();
+    };
+  }, []);
+
+  return <div id="firebaseui-auth-container"></div>;
+};
+
+
